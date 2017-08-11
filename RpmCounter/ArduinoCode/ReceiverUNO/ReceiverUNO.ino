0716bbaa0396edd85a5b5e397552f84aaf432533
Andrea <andrea@andrea.local>
2017-08-11 10:47:27 +0900

    Added receiver code

---
+++ RpmCounter/ArduinoCode/ReceiverUNO/ReceiverUNO.ino
@@ -0,0 +1,21 @@
+#include "Wire.h"
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(1); 
+  Wire.onReceive(receiveEvent);
+}
+
+
+void receiveEvent(int bytes) {  
+  if (bytes != 2) return;
+  int lower= Wire.read();
+  int upper= Wire.read();
+  
+  Serial.println(lower | (upper <<8));
+}
+
+
+void loop() {
+  
+}
---
+++ RpmCounter/ArduinoCode/libraries/TinyWire/examples/attiny85_i2c_simpleSlave/attiny85_i2c_simpleSlave.ino
@@ -0,0 +1,34 @@
+#define I2C_SLAVE_ADDRESS 8 
+#include <TinyWireSlave.h>
+
+
+void requestEvent()
+{  
+    TinyWireS.send(counter);
+    counter++;
+}
+
+
+void receiveEvent(uint8_t howMany)
+{
+  // on request
+}
+
+
+void setup()
+{
+    TinyWireS.begin(I2C_SLAVE_ADDRESS);
+    TinyWireS.onReceive(receiveEvent);
+    TinyWireS.onRequest(requestEvent);
+}
+
+
+void loop()
+{
+    /**
+     * This is the only way we can detect stop condition (http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&p=984716&sid=82e9dc7299a8243b86cf7969dd41b5b5#984716)
+     * it needs to be called in a very tight loop in order not to miss any (REMINDER: Do *not* use delay() anywhere, use tws_delay() instead).
+     * It will call the function registered via TinyWireS.onReceive(); if there is data in the buffer on stop.
+     */
+    TinyWireS_stop_check();
+}
